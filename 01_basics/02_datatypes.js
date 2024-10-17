72075edd6e0516ce3d68ce224284775a069eba25
Aditi Raghuwanshi <130664278+AditiRaghuwanshi@users.noreply.github.com>
2024-10-17 18:54:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";  //treates all js codes as newer version
+// alert("hellloo");
+
+//number => 2 to power 53
+//string => ""
+// boolean => true / false
+// null => standalone value
+// symbol => unique 
+// undefined
+
+console.log(typeof null);
+console.log(typeof undefined);
