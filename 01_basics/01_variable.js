0cca7018c07e5db46c43f2421db57860992a47ae
Aditi Raghuwanshi <130664278+AditiRaghuwanshi@users.noreply.github.com>
2024-10-17 18:36:33 +0000

    container commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "admin123@gmail.com";
+var accountNumber = "2343123465";
+accountCity = "indore";
+let accountState;
+
+//const ko change nai krskte 
+// accountId = 2;
+accountEmail = "admin@123"
+accountCity =" bhopal"
+console.log(accountEmail);
+
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountNumber, accountCity, accountState]);
+
+/* not to use var bcoz of block and functional scope*/ 
+/* jb hum varible ka type nai btate h then wo undefined jatae in js */
