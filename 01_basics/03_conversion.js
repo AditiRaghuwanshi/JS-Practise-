0c89a7b808295ce18fc4ad0672cb1d6b7beba3fd
Aditi Raghuwanshi <130664278+AditiRaghuwanshi@users.noreply.github.com>
2024-10-18 03:51:02 +0000

    converion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,37 @@
+let score = "33ndn";
+console.log(typeof score);
+console.log((typeof score));
+
+let valueInNumber = Number(score)
+console.log((typeof valueInNumber));
+console.log(valueInNumber);
+
+let isloggedIn = "";
+let istrueorfalse = Boolean(isloggedIn);
+console.log(istrueorfalse);
+
+
+//NULL - FALSE ME 
+//undefined - false me
+//string = true me
+//"" = false
+// 1- true 0-false
+
+// let somenumber = undefined;
+// let stringnumber = String(somenumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// string will convert to string 
+// empty string me kuch bhi ni aya
+// null me null hi but converted in string in typeof
+
+let some = "";
+let objectconvert = Object(some);
+console.log(objectconvert);
+console.log(typeof object);
+
+//undefined convert object -> {}
+//string -> undefined 
+
+
