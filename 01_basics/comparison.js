09f14179f16ae5b08a189af0e3bebbbcb89c071b
Aditi Raghuwanshi <130664278+AditiRaghuwanshi@users.noreply.github.com>
2024-10-18 04:22:00 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,35 @@
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined >= 0);
+
+// == all work differently but >= is like pehle usne convert kra null ko zero me and then zero zero equal 
+// == , === and comparison work differently comparison and equality work differently
+//=== equality + datatype
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+/*********************operations********************* */
+
+console.log("1" + 2);
+console.log(true);
+console.log(2+1+"3");
+console.log(+true);
+console.log(-false);
+
+// console.log(false-);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 4;
+console.log(num1);
