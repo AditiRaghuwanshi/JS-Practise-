b55ed9e9f4fa579e18c7961a26141b7cdc10fd2d
Aditi Raghuwanshi <130664278+AditiRaghuwanshi@users.noreply.github.com>
2024-10-24 03:52:32 +0000

    arrow

---
+++ Arrow/arrow.js
@@ -0,0 +1,57 @@
+//This keyword current context k baare m btata hain
+
+// const user ={
+//     name: "aditi",
+//     price: "10000",
+
+//     welcomeMessage: function() {
+//         console.log(`${this.name}, welcome to website`);
+//         // console.log(this)
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "hello"
+// user.welcomeMessage()
+// console.log(this)
+
+// user.welcomeMessage()
+
+
+//arrow function
+
+// function chai() {
+//     let username = "aditi"
+//     console.log(this.username);
+// }
+// chai()
+
+
+// const chai = () => {
+//     let user = "aditi";
+//     console.log(this);
+
+// }
+
+// chai()
+
+
+// const addone = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// console.log(addone(2,4));
+
+
+//implicit return 
+
+// const addone = (num1, num2) =>  (num1 + num2);
+// const addone = (num1, num2) =>  num1 + num2;
+
+console.log(addone(2,4));
+
+//implicit w/o return
+//explicit with return
+
+
+
